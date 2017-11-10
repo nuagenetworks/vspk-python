6edfe5fdaebfa72f5154f5b2436629530fa0e798
siebeclaes <siebe.claes@gmail.com>
2017-11-10 10:27:06 +0100

    CLI: add more invariant resources

--- vspk/cli/utils.py
+++ vspk/cli/utils.py
@@ -38,7 +38,7 @@ class Utils(object):
     """ Utils """
 
     # TODO: remove that custom thing
-    INVARIANT_RESOURCES = ["qos", "vrs", "cms", "statistics", "licensestatus"]
+    INVARIANT_RESOURCES = ["qos", "vrs", "cms", "statistics", "licensestatus", "vrsmetrics", "ltestatistics", "bulkstatistics"]
 
     @classmethod
     def _clean_name(cls, string):
