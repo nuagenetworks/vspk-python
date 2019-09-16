18a1f03396077f8399aa68377e92b9a48593a2b1
nuagebot <corentin.henry@nokia.com>
2019-09-16 08:18:20 +0000

    Auto generated from specifications change.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ for version_folder in os.listdir(api_version_path):
 
 setup(
     name='vspk',
-    version="6.0.1",
+    version="6.0.2",
     url='http://nuagenetworks.net/',
     author='nuage networks',
     author_email='opensource@nuagenetworks.net',
